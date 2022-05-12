db7f39f7a28a5cdbd385616d07064dd8e8a063a4
Svetlana Asotikova <asotikova@devgroup.ru>
2022-05-12 17:52:56 +0300

    update styles

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -3,6 +3,8 @@ import { useSelector, useDispatch } from "react-redux";
 import styles from '../styles/NavItem.module.scss';
 import UserFolder from "./UserFolder";
 import { showModal } from "../store/actions/actionCreators";
+import { ReactComponent as Arrow } from '../assets/icons/arrow.svg';
+import { ReactComponent as Plus } from '../assets/icons/plus.svg';
 
 const NavItem = ({ folder, handleClickFolder, userFolders }) => {
   const [active, setActive] = useState(false);
@@ -36,14 +38,14 @@ const NavItem = ({ folder, handleClickFolder, userFolders }) => {
             <span
               className={`${active ? `${styles.transform}` : ''}`}
             >
-              {'>'}
+              <Arrow />
             </span>
             {folder.name}
             <span
               className={styles.modal}
               onClick={handleModal}
             >
-              +
+              <Plus />
             </span>
           </div>
           <ul className={`${styles.item__userFolder} ${active ? `${styles.show}` : `${styles.hide}`}`}>
